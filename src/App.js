38a85db3480fe01d3481901d609aec556a998dee
obayalhusein <obayalhusein@gmail.com>
2022-11-16 18:27:25 +0300

    add homepage component / add default layout component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Home from "./pages/home";
+
 function App() {
   return (
     <div>
+      {/* I would use react-router if there is more pages */}
+      <Home />
     </div>
   );
 }
---
+++ src/layouts/default.js
@@ -0,0 +1,11 @@
+const Layout =({children}) =>{
+    return(
+        <>
+            <header>header</header>
+            <main>{children}</main>
+            <footer>footer</footer>
+        </>
+    )
+}
+
+export default Layout;
