8364c6d83ffa244bf433d862f92cc615b4de4df1
obayalhusein <obayalhusein@gmail.com>
2022-11-19 05:04:55 +0300

    add sass loader / add basic scss files / add intro section component

--- src/components/sections/IntroSection.jsx
+++ src/components/sections/IntroSection.jsx
@@ -4,10 +4,10 @@ import { Box } from "@mui/system";
 export default function IntroSection() {
   return (
     <section>
-      <Grid container alignItems="center" spacing={2}>
+      <Grid container alignItems="center">
         <Grid xs={6}>
-          <Typography variant="h2" component="h1">
-            <Box fontWeight="fontWeightMedium" component="span">
+          <Typography variant="h1" component="h1">
+            <Box fontWeight="fontWeightBold" component="span">
               More than just shorter links
             </Box>
           </Typography>
@@ -16,7 +16,7 @@ export default function IntroSection() {
             Build your brand's recognition and get detailed insights on how your links are performing
           </Typography>
 
-          <Button variant="contained">Get Started</Button>
+          <Button variant="contained" sx={{ mt: 3 }}>Get Started</Button>
         </Grid>
         <Grid xs={6}>
           <img src="/images/illustrations/illustration-working.svg" />
